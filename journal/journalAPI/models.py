cc3c48bfbc3f3e8625d061e84e5669edfcf61755
Edison Wang <ew040607@gmail.com>
2025-09-05 14:46:48 -0400

    added phone number to user model

--- journal/journalAPI/models.py
+++ journal/journalAPI/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid
-
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
+    phone_number = PhoneNumberField()
 
 class Capsule(models.Model):
     capsule_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
