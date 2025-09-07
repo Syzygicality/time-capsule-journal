552fc9ce49b218ef96e31ccdba32b0c506474a62
Edison Wang <ew040607@gmail.com>
2025-09-07 15:00:11 -0400

    added draft to admin

--- journal/journalAPI/admin.py
+++ journal/journalAPI/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User, Capsule
+from .models import User, Capsule, Draft
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Capsule)
+admin.site.register(Draft)
