de25ebb34c91018f0ba296dde5b26617a3c6b90b
Edison Wang <ew040607@gmail.com>
2025-09-09 14:11:57 -0400

    added encryption to serializer and removed read only fields for easier development and testing

--- journal/journalAPI/serializers.py
+++ journal/journalAPI/serializers.py
@@ -1,24 +1,31 @@
 from rest_framework import serializers
 from .models import User, Capsule, Draft
+from utilities.encryption import encrypt_message, decrypt_message
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["email", "username", "first_name", "last_name"]
+        fields = ["email", "username", "first_name", "last_name", "phone_number"]
 
 class CapsuleSerializer(serializers.ModelSerializer):
-    release_date = serializers.SerializerMethodField
-    
+    message = serializers.CharField(write_only=True)
+    decrypted_message = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Capsule
-        read_only_fields = ["message", "creation_date", "release_date"]
+        fields = ["capsule_id", "message", "decrypted_message", "creation_date", "release_date"]
+
+    def create(self, validated_data):
+        raw_message = validated_data.pop("message")
+        validated_data["encrypted_message"] = encrypt_message(raw_message)
+        return super().create(validated_data)
     
-    def get_release_date(self, capsule):
-        return capsule.creation_date + capsule.locked_time
+    def get_decrypted_message(self, capsule):
+        return decrypt_message(capsule.encrypted_message)
+
 
 class DraftSerializer(serializers.ModelSerializer):
     class Meta:
         model = Draft
-        fields = ["message", "locked_time"]
-        read_only_fields = ["creation_date", "modified_date"]
+        fields = ["message", "locked_time", "creation_date", "modified_date"]
     
