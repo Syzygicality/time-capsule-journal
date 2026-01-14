fdd2c9fac2c688fda95c016d1825a8230c9735f0
Edison Wang <ew040607@gmail.com>
2026-01-14 18:27:44 -0500

    create conversation model

--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ class User(SQLModel, table=True):
 
     api_key: Optional["APIKey"] = Relationship(back_populates="user", sa_relationship_kwargs={"uselist": False})
     capsules: List["Capsule"] = Relationship(back_populates="user")
+    conversations: List["Conversation"] = Relationship(back_populates="user")
 
 class APIKey(SQLModel, table=True):
     id: UUID = Field(primary_key=True, default_factory=uuid4)
@@ -29,12 +30,26 @@ class APIKey(SQLModel, table=True):
 class Capsule(SQLModel, table=True):
     id: UUID = Field(primary_key=True, default_factory=uuid4)
     user_id: UUID = Field(foreign_key="user.id", ondelete="CASCADE", index=True)
+    conversation_id: Optional[UUID] = Field(foreign_key="conversation.id", nullable=True, index=True, default=None)
     content: str = Field(max_length=250)
     creation_date: datetime = Field(default_factory=current_time)
     time_held: timedelta = Field()
     release_date: datetime = Field()
     replying_to_id: Optional[UUID] = Field(default=None, foreign_key="capsule.id", nullable=True)
-    reply_allowed: bool = Field(default=True)
 
     user: "User" = Relationship(back_populates="capsules")
+    conversation: Optional["Conversation"] = Relationship(back_populates="capsules")
     replying_to: Optional["Capsule"] = Relationship(sa_relationship_kwargs={"remote_side": "Capsule.id"})
+
+class Conversation(SQLModel, table=True):
+    id: UUID = Field(primary_key=True, default_factory=uuid4)
+    user_id: UUID = Field(foreign_key="user.id", ondelete="CASCADE", index=True)
+    latest_capsule_id: Optional[UUID] = Field(foreign_key="capsule.id", nullable=True)
+
+    user: "User" = Relationship(back_populates="conversations")
+    capsules: List["Capsule"] = Relationship(back_populates="conversation")
+    latest_capsule: Optional["Capsule"] = Relationship(sa_relationship_kwargs={"remote_side": "Capsule.id"})
+
+    @property
+    def reply_allowed(self):
+        return current_time() > self.latest_capsule.release_date
